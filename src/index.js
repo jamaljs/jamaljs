f5d68ba3da8f0b0e7f791481235876b3d585556e
Akın Özgen <akinozgen17@outlook.com>
2018-11-14 20:10:37 +0300

    first external module. idk how to implement a bundler

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const jml = function (selector, markup) {
 };
 */
 
+import uniqid from 'uniqid';
 
 const tags = "a,abbr,acronym,address,applet,area,article,aside,audio,b,base,basefont,bdi,bdo,big,blockquote,body,br,button,canvas,caption,center,cite,code,col,colgroup,datalist,dd,del,details,dfn,dialog,dir,div,dl,dt,em,embed,fieldset,figcaption,figure,font,footer,form,frame,frameset,h1,h2,h3,h4,head,header,hr,html,i,iframe,img,input,ins,kbd,keygen,label,legend,li,link,main,map,mark,menu,menuitem,meta,meter,nav,noframes,noscript,object,ol,optgroup,option,output,p,param,picture,pre,progress,q,rp,rt,ruby,s,samp,script,section,select,small,source,span,strike,strong,style,sub,summary,sup,table,tbody,td,textarea,tfoot,th,thead,time,title,tr,track,tt,u,ul,video,wbr".split(',');
 
@@ -32,8 +33,17 @@ Jml.create = function (selector, markup, debug = false) {
 	};
 }
 
-Jml.initialize = ({ debug = false }) => {
+Jml.initialize = ({ debug = false, customTags = [] }) => {
 	if (debug) console.log('Jml debug mode is on 💚');
+
+	if (customTags.length > 0) {
+		customTags.forEach(customTag => tags.push((_ => {
+			if (!_.includes('-')) return _;
+
+			const parts = customTag.split('-').map(x => x.replace(/^./, fc => fc.toUpperCase()));
+			return parts.join('');
+		})(customTag)));
+	}
 
 	tags.forEach(tag => {
 		window[`j${tag.replace(/^./, firstCharacter => firstCharacter.toUpperCase())}`] = (attrs, children) => {
@@ -55,6 +65,8 @@ Jml.initialize = ({ debug = false }) => {
 				console.log('\t\tChildren:', debugChildren);
 				console.groupEnd();
 			}
+
+			window[`${tag}-ref-${uniqid()}`] = '';
 
 			return `\n<${tag}${parsedAttributes ? ' ' + parsedAttributes : ''}>${parsedChildren}</${tag}>`;
 		};
